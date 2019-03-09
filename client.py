cf36bb88cee8d73c3dd9f15599c26bf5b80905ca
BooleanWater <48110588+BooleanWater@users.noreply.github.com>
2019-03-09 15:15:45 +0800

    UDP的简单示例
    UDP的简单示例

--- client.py
+++ client.py
@@ -1,11 +1,6 @@
 import socket
-s=socket.socket()
-host='127.0.0.1'
-port=8080
-s.connect((host,port))
-while True:
-    send_data=input("请输入要发送的数据： ")
-    s.send(send_data.encode())
-    recvData=s.recv(1024).decode()
-    print('接收到的数据为： ',recvData)
-#s.close()
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+data=input('请输入要转换的温度(单位：摄氏度)：')
+s.sendto(data.encode(),('127.0.0.1',8888))
+print(s.recv(1024).decode())
+s.close()
